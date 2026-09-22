41924d5281cdb12de41201dd28f4601182f2b3c1
Velvet-Stories26 <sinamikas926@gmail.com>
2026-09-22 12:44:13 +0530

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/wedding-invitation2/",
+  base: "/Wedding-invitation2/",
   resolve: {
     tsconfigPaths: true,
   },
